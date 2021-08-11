291e165c74d4076e985662d80f0994c58d76178a
hyeminLee7 <hmin10580@gmail.com>
2021-08-11 17:46:55 +0900

    0811

--- src/main/java/shop/Jarvis/app/demo/DemoApplication.java
+++ src/main/java/shop/Jarvis/app/demo/DemoApplication.java
@@ -1,13 +1,39 @@
 package shop.Jarvis.app.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import shop.Jarvis.app.demo.calculator.entity.User;
+import shop.Jarvis.app.demo.calculator.repository.UserRepository;
+
+import java.util.List;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+	@Autowired
+	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.deleteAll();
+		userRepository.save(new User("aa","장영실","aa@","1"));
+		userRepository.save(new User("bb","장영실","bb@","1"));
+		userRepository.save(new User("cc","전우치","cc@","1"));
+
+		List<User> users = userRepository.findAll();
+		for(User u: users){
+			System.out.println(u.toString());
+		}
+		System.out.println("장영실만 출력");
+		for(User u:userRepository.findAllByName("장영실")){
+			System.out.println(u.toString());
+		}
+	}
 }
+
---
+++ src/main/java/shop/Jarvis/app/demo/calculator/entity/User.java
@@ -0,0 +1,31 @@
+package shop.Jarvis.app.demo.calculator.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Id;
+
+@Document(collection = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+    @Id
+    private String userId;
+    private String name;
+    private String email;
+    private String password;
+
+    @Override
+    public String toString() {
+        return "회원명세{" +
+                "아이디='" + userId + '\'' +
+                ", 이름='" + name + '\'' +
+                ", 이메일='" + email + '\'' +
+                ", 비밀번호='" + password + '\'' +
+                '}';
+    }
+}
+
