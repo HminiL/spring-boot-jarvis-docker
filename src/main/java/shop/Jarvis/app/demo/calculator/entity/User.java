9596b7a09e2f7136d3ddfcddb2ffd0fedbb02fea
hyeminLee7 <hmin10580@gmail.com>
2021-08-11 17:58:23 +0900

    0811

--- src/main/java/shop/Jarvis/app/demo/calculator/entity/User.java
+++ src/main/java/shop/Jarvis/app/demo/calculator/entity/User.java
@@ -3,9 +3,9 @@ package shop.Jarvis.app.demo.calculator.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.Id;
 
 @Document(collection = "users")
 @AllArgsConstructor
--- src/test/java/shop/Jarvis/app/demo/calculator/service/CalculatorServiceImplTest.java
+++ src/test/java/shop/Jarvis/app/demo/calculator/service/CalculatorServiceImplTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
