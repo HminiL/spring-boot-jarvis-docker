57efd05f029d6546e8d0f4ac6cdce5946ba2a8b9
hyeminLee7 <hmin10580@gmail.com>
2021-08-12 16:50:16 +0900

    0812..

--- src/main/java/shop/Jarvis/app/demo/calculator/repository/ItemRepository.java
+++ src/main/java/shop/Jarvis/app/demo/calculator/repository/ItemRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import shop.Jarvis.app.demo.calculator.entity.Item;
 
+import java.util.List;
+
 @Repository
 public interface ItemRepository extends MongoRepository<Item, Long> {
+    public List<Item> findAllByItemName(String itemName);
 }
